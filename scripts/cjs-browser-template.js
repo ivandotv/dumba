030d5ba545d8f44ee988a98e2038624b6d89d3b4
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2021-12-08 22:23:16 +0100

    switch to microbundle build

--- scripts/cjs-browser-template.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./__LIBRARY_NAME__.production.min.js')
-} else {
-  module.exports = require('./__LIBRARY_NAME__.development.js')
-}
